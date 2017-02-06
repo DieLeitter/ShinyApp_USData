e6ca58bfcfc10e082ce4cfdd65126525669b4c79
DieLeitter <malwina.staniuk@gmail.com>
2017-02-06 23:40:18 +0100

    Updated

--- index.md
+++ index.md
@@ -6,9 +6,6 @@ autosize: true
 transition: rotate
 transition-speed: fast
 
-
-
-
 About
 ========================================================
 
@@ -22,9 +19,9 @@ The code for the ui. R and server.R is avaliable at Github: <https://github.com/
 Introduction
 ========================================================
 * Wondering if there is correlation between income and education?  
-* Does Us states with high Education level have lower murder rate?
-* Does the average frost number of days correlate with population? 
-* Does the life expextancy correlete with people illiteracy?
+* Do states of USA with high education level have lower murder rate?
+* Does the average of frost days correlate with population? 
+* Does the life expextancy collerate with illiteracy?
 
 <br>
 <br>
@@ -37,41 +34,26 @@ Introduction
 USA Data Shiny App
 ========================================================
 
-This shiny app generates scatter plots between two chosen variables and fit a linear regression. 
-The varaibles are selected form state.x77 dataset (more info about data set here <https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html>). One can choose between 8 features: 
-Population, Income, Illiteracy, Life Exp., Murder, HS Grad, Frost, and Area.
+<small> This shiny app generates scatter plots between two chosen variables and fit a linear regression. 
+The varaibles are selected from `state.x77` dataset (more info about data set here <https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html>). One can choose between 8 features: 
+Population, Income, Illiteracy, Life Exp., Murder, HS Grad, Frost, and Area.</small>
 
 To get started:
 * Select desired x and y from the input boxes. 
-* Brush the plot to select points for which you would like to fit regression line, which will be seen as a blue line in the plot. Note: at least two points needs to be selected to fit a regression line.
-* Zoom in can be done by selecting and area (brushing) an double clicking.
+* Brush the plot to select points for which you would like to fit regression line. The regression line will be added as a blue line in the plot. Note: at least two points need to be selected to fit a regression line.
+* Zoom in can be done by double clicking on previously selected (brushed) area.
 
 
 
+
+Scatter plot example
 ========================================================
-left: 35%
 
-Scatterplot example
-![plot of chunk unnamed-chunk-1](index-figure/unnamed-chunk-1-1.png)
 
-***
+<img src="index-figure/unnamed-chunk-1-1.png" title="plot of chunk unnamed-chunk-1" alt="plot of chunk unnamed-chunk-1" style="display: block; margin: auto;" />
 
-Few lines of the state.x77 dataset:
-
-```r
-state.x77[1:3, 1:4]
-```
-
-```
-        Population Income Illiteracy Life Exp
-Alabama       3615   3624        2.1    69.05
-Alaska         365   6315        1.5    69.31
-Arizona       2212   4530        1.8    70.55
-```
-<br>
-<br>
-<br>
 <br>
 Caution:
-<span style="font-weight:bold; color:red;">Correlation is not causation.</span>
+<span style="font-weight:bold; color:red;">Correlation is not causation</span>
+
 
--- ui.R
+++ ui.R
@@ -4,6 +4,7 @@ shinyUI(fluidPage(
         titlePanel("Linear Regression for USA data (data:state.x77)"),
         sidebarLayout(
                 sidebarPanel(
+                        br(),
                         h3("Choose x (predictors) and y (outcome)"),
                         selectInput("select_x", label=h5("Select x"),
                                     choices=list("Population",
@@ -25,6 +26,7 @@ shinyUI(fluidPage(
                                                  "Frost",
                                                  "Area"),
                                     selected="Illiteracy"),
+                        br(),br(),
                         h3("Linear model"),
                         h5("(Brush points to see fitted model)"),
                         h5("Slope"),
@@ -34,24 +36,38 @@ shinyUI(fluidPage(
                         
                         br(),
                         checkboxInput("checkbox", label="Show equation of linear regression", value=FALSE),
-                        
-        
-                        
-                        br(),br(),
-                        h5("Click link below to see documentataion for state.x77 dataset"),
-                        a("state.x77", target="_blank", href="https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html"),
-                        br(), br(),
-                        h5("For a presentation pitch click link below"),
-                        a("Presentation_RPubs", href="http://rpubs.com/ooctig/247153")
-                
+                        br()
                 ),
+                
                 mainPanel(
-                        plotOutput("plot1", width = "100%", brush=brushOpts(id="brush1"),
-                                   dblclick = "plot1_dblclick")
+                        tabsetPanel(type="tabs",
+                                    tabPanel("Main",
+                                             plotOutput("plot1", width = "100%", brush=brushOpts(id="brush1"),
+                                                        dblclick = "plot1_dblclick")
+                                    ),
+                                    tabPanel("About", br(), br(),
+                                             h5("This shiny app generates scatter plots between two chosen variables and fit a linear regression. 
+                                                The varaibles are selected from `state.x77` dataset. One can choose between 8 features: 
+                                                Population, Income, Illiteracy, Life Exp., Murder, HS Grad, Frost, and Area."),
+                                             br(), br(),
+                                             
+                                             h5("Click link below to see documentataion for state.x77 dataset"),
+                                             a("state.x77", target="_blank", href="https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html"),
+                                             br(), br()
+                                    ),
+                                    tabPanel("How to start", br(), br(),
+                                             tags$ul(
+                                                     tags$li("Select desired x and y from the input boxes."), 
+                                                     tags$li("Brush the plot to select points for which you would like to fit regression line. The regression line will be added as a blue line in the plot. Note: at least two points need to be selected to fit a regression line."), 
+                                                     tags$li("Zoom in can be done by double clicking on previously selected (brushed) area.")
+                                             ),
+                                             br(),br(),
+                                             h5("For a presentation pitch click link below"),
+                                             a("Presentation_RPubs", href="http://rpubs.com/ooctig/247740")
+                                    )                                    
                         )
+                )
         )
-        
-        
 ))
 
 
